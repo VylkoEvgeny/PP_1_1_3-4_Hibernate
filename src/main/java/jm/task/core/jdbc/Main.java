eea810a58b59ff882b34f6ec604fd2b029aff041
Evgeny Vylko <belfoot10@gmail.com>
2026-05-04 14:39:29 +0300

    create Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Ivan", "Ivanov", (byte) 25);
+        userService.saveUser("Petr", "Petrov", (byte) 30);
+        userService.saveUser("Anna", "Smirnova", (byte) 22);
+        userService.saveUser("Olga", "Sidorova", (byte) 28);
+        List<User> allUsers = userService.getAllUsers();
+        allUsers.forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
