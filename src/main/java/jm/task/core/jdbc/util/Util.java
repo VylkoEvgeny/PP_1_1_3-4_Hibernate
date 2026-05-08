e253fe2e47c39f9197478e57ab1d9beb835d72c0
Evgeny Vylko <belfoot10@gmail.com>
2026-05-08 13:41:15 +0300

    create configuration for Hibernate in Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,14 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
+    /* Код для JDBC
     private static final String URL = "jdbc:mysql://localhost:3306/userdb";
     private static final String USER = "root";
     private static final String PASSWORD = "6374985";
@@ -14,5 +18,17 @@ public class Util {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+     */
+    private static SessionFactory sessionFactory;
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            Configuration conf = new Configuration();
+            conf.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/userdb");
+            conf.setProperty("hibernate.connection.username", "root");
+            conf.setProperty("hibernate.connection.password", "6374985");
+            conf.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        }
+        return sessionFactory;
     }
 }
