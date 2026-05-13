894eac1c1d952b378cda28327a59b760fac13b4f
Evgeny Vylko <belfoot10@gmail.com>
2026-05-13 15:05:44 +0300

    create methods in UserDaoHibernateImpl

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,118 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
 
+    private static final Logger logger = Logger.getLogger(UserDaoHibernateImpl.class.getName());
+    private SessionFactory sessionFactory;
+
+    public UserDaoHibernateImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
-
 
     @Override
     public void createUsersTable() {
-
+        String sql = "create table if not exists users (id bigint auto_increment primary key, name varchar(255), lastName varchar(255), age tinyint);";
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            session.createNativeQuery(sql).executeUpdate();
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            logger.severe("Произошла ошибка: " + e.getMessage());
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        String sql = "drop table if exists users;";
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            session.createNativeQuery(sql).executeUpdate();
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            logger.severe("Произошла ошибка: " + e.getMessage());
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.save(user);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            logger.severe("Произошла ошибка: " + e.getMessage());
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            User user = session.get(User.class, id);
+            if (user != null) {
+                session.delete(user);
+            }
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            logger.severe("Произошла ошибка: " + e.getMessage());
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Transaction tx = null;
+        List<User> fromUser = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            fromUser = session.createQuery("from User", User.class).getResultList();
+            tx.commit();
+
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            logger.severe("Произошла ошибка: " + e.getMessage());
+        }
+        return fromUser;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Transaction tx = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            session.createQuery("delete from User").executeUpdate();
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            logger.severe("Произошла ошибка: " + e.getMessage());
+        }
     }
 }
